7f21d982c46a7ad8f98ab2272dbe0f7b295cab4e
SG <ilovesoccer3939@gmail.com>
2025-11-11 12:34:04 +0900

    [update] BoardCreatorの実装

--- Assets/Scripts/Entity/Domain/InGame/Puzzle/BoardCreator.cs
+++ Assets/Scripts/Entity/Domain/InGame/Puzzle/BoardCreator.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Entity.Domain.InGame.Core;
 using UnityEngine;
 
@@ -8,20 +9,60 @@ namespace Entity.Domain.InGame.Puzzle
         private int width;
         private int height;
         private DropType[,] grid;
-        private ComboChecker checker;
+        private DropType[] dropArray;
         public BoardCreator(Board board)
         {
             grid = board.GetGrid();
             width = board.GetGrid().GetLength(0);
             height = board.GetGrid().GetLength(1);
 
-            checker = new ComboChecker();
+            dropArray = new DropType[]
+            {
+                DropType.SingleAttack,
+                DropType.MultiAttack,
+                DropType.Heal,
+                DropType.Shield
+            };
         }
         public Board CreateBoard(Board board)
         {
-            var result = checker.CheckCombo();
+            var generateBord = FactoryBorad(board);
+            return generateBord;
+        }
+        private Board FactoryBorad(Board board)
+        {
+            for(int i = 0; i < width; i++)
+            {
+                for(int j = 0; j < height; j++)
+                {
+                    int random= Random.Range(0, dropArray.Length);
+                    grid[i,j]=dropArray[random];
 
-            return null;
+                    for(int _checkCount = 0; _checkCount < 4; _checkCount++)
+                    {
+                        if (CheckSameDropLine(i, j, board)) break;
+                        random = Random.Range(0, dropArray.Length);
+                        grid[i, j] = dropArray[random];
+                    }
+                }
+            }
+            board.SetGrid(grid);
+            return board;
+        }
+        /// <summary>
+        /// �R���{���Ă��Ȃ��̃`�F�b�N
+        /// </summary>
+        private bool CheckSameDropLine(int x,int y,Board board)
+        {
+            DropType current = grid[x, y];
+
+            if (x >= 2 && grid[x - 1, y] == current && grid[x - 2, y] == current)
+                return false;
+
+            if (y >= 2 && grid[x, y - 1] == current && grid[x, y - 2] == current)
+                return false; 
+            //�����Ɉ���������Ȃ����true��Ԃ�
+            return true;
         }
     }
 }
